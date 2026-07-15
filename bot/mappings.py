4c363567cc0965607afb861697bb262fe2ff9c39
andre <markov3687@gmail.com>
2026-07-15 10:01:33 +0400

    Fix patent issue date and validity start date in Conclusion/Termination documents

--- bot/mappings.py
+++ bot/mappings.py
@@ -26,7 +26,10 @@ CONTRACT_DATE_CELLS = ([1, 2], [4, 5], [8, 9, 10, 11], [3, 6, 7])
 
 # Specific cell mappings for Conclusion / Termination notifications
 CONCL_PASSPORT_DATE_CELLS = ([20, 21], [23, 24], [26, 27, 28, 29], [22, 25])
+# T33 R1: [20]=D1 [21]=D2 [22]=. [23]=M1 [24]=M2 [25]=. [26]=Y1 [27]=Y2 [28]=Y3 [29]=Y4
 CONCL_PATENT_DATE_CELLS = ([20, 21], [23, 24], [26, 27, 28, 29], [22, 25])
+# T36 R0 start: [1]=D1 [2]=D2 [3]=. [4]=M1 [5]=M2 [6]=. [7]=Y1..Y4
+# T36 R0 end:   [12]=D1 [13]=D2 [14]=. [15]=M1 [16]=M2 [17]=. [18]=Y1..Y4
 CONCL_PATENT_VALIDITY_START_CELLS = ([1, 2], [4, 5], [7, 8, 9, 10], [3, 6])
 CONCL_PATENT_VALIDITY_END_CELLS = ([12, 13], [15, 16], [18, 19, 20, 21], [14, 17])
 CONCL_CONTRACT_DATE_CELLS = ([1, 2], [4, 5], [7, 8, 9, 10], [3, 6])
